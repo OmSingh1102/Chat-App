a8fee51debaafd1d32dd9730f094e83391cdbdde
Om Singh <oms455305@gmail.com>
2024-09-04 22:59:55 +0530

    typo

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
-import cors from "cors";
-const dotenv = require("dotenv");
 
+const dotenv = require("dotenv");
+const cors = require("cors")
 const { chats } = require("./data/data");
 const path = require('path')
 const userRoutes = require("./routes/userRoutes");
